0d9772e6e2571cf47fa1a1de9c0dfcf0c94a532c
Natan-Henrique <natanhcr7@gmail.com>
2025-11-01 19:30:11 -0300

    commit inicial

---
+++ caminhao_main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+void DadosSensor() {
+    int i_posicao_x = 0;
+    int i_posicao_y = 0;
+    int i_angulo_x = 0;
+    
+}
+
+
+
+
+
+int main()  {
+    
+}
